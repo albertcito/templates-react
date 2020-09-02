b203aa875e992c1220b200c99624864df1585d64
Albert Barrientos <albert.bar@gmail.com>
2020-09-02 13:54:29 -0400

    lint ts errores fixed

--- src/routes/interfaces.tsx
+++ src/routes/interfaces.tsx
@@ -1,19 +1,19 @@
-import AppPageInterface from './PageProperties';
+import PageProperties from './PageProperties';
 import LayoutPageProperties from '../templates/interfaces/LayoutPageProperties';
 import { RouteTypeEnum } from './routeTypes';
 
 export interface RouteProperties {
-  component: React.SFC<AppPageInterface>;
+  component: React.FC<PageProperties>;
   exact?: boolean;
   params?: {
     [key: string]: any;
   };
   path: string;
-  template?: React.SFC<LayoutPageProperties>;
+  template?: React.FC<LayoutPageProperties>;
 }
 
 export interface RouteTemplateProperties {
   routes: RouteProperties[];
-  template: React.SFC<LayoutPageProperties>;
+  template: React.FC<LayoutPageProperties>;
   type: RouteTypeEnum;
 }
